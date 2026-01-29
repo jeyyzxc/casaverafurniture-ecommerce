73fecd2518543fb434609abdefb6b5a19e41e6d3
jeyyzxc_ <jeromesanchezregoya@gmail.com>
2026-01-29 21:29:00 +0800

    fix errors in fetching data

--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -39,9 +39,9 @@ return [
         ],
 
         'public' => [
-            'driver' => 'local',
+            'driver' => 'cloudinary', // Changed from 'local' to 'cloudinary'
             'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
